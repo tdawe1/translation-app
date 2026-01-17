dfe751dc6b01c73d869155c29bfae5c3dc8b2c18
tdawe1 <thomas@kyros.solutions>
2026-01-17 14:44:00 +0000

    i18n: complete internationalization infrastructure
    Frontend:
    - Add LanguageSelector component (accessible, responsive, matches design system)
    - Migrate AuthForm to use next-intl translations
    - Language dropdown with flags, ARIA labels, click-outside handling
    
    Backend:
    - Install github.com/nicksnyder/go-i18n/v2 library
    - Create i18n helper package (localizer.go)
    - Add translation files for 4 languages (en, es, fr, de)
    - Translation keys for: common, auth, watcher, validation, server
    
    Features:
    - Locale routing via middleware (localePrefix: 'always')
    - JSON translation files with nested keys
    - Type-safe localization helpers
    - Accept-Language header parsing support

---
+++ backend/internal/i18n/localizer.go
@@ -0,0 +1,92 @@
+package i18n
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+var (
+	bundle     *i18n.Bundle
+	bundleOnce sync.Once
+	English    = language.English
+	Spanish    = language.Spanish
+	French     = language.French
+	German     = language.German
+)
+
+// Init initializes i18n bundle
+func Init() error {
+	var initErr error
+	bundleOnce.Do(func() {
+		bundle = i18n.NewBundle(language.English)
+		bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+
+		for _, lang := range []string{"en", "es", "fr", "de"} {
+			path := filepath.Join("i18n", lang, fmt.Sprintf("active.%s.json", lang))
+			data, err := os.ReadFile(path)
+			if err != nil {
+				initErr = fmt.Errorf("failed to read translation file %s: %w", path, err)
+				return
+			}
+			if _, err := bundle.ParseMessageFileBytes(data, path); err != nil {
+				initErr = fmt.Errorf("failed to parse translation file %s: %w", path, err)
+				return
+			}
+		}
+	})
+	return initErr
+}
+
+// Localizer returns a localizer for given language tag
+func Localizer(tag language.Tag) *i18n.Localizer {
+	if bundle == nil {
+		if err := Init(); err != nil {
+			return i18n.NewLocalizer(bundle, language.English.String())
+		}
+	}
+	return i18n.NewLocalizer(bundle, tag.String())
+}
+
+// GetLocalizedMessage returns a localized message with given key and template data
+func GetLocalizedMessage(tag language.Tag, key string, templateData map[string]interface{}) string {
+	loc := Localizer(tag)
+	msg, err := loc.Localize(&i18n.LocalizeConfig{
+		MessageID:    key,
+		TemplateData: templateData,
+	})
+	if err != nil {
+		return key
+	}
+	return msg
+}
+
+// GetLocalizedMessagef is a convenience function that formats message with variadic args
+func GetLocalizedMessagef(tag language.Tag, key string, args ...interface{}) string {
+	loc := Localizer(tag)
+	msg, err := loc.Localize(&i18n.LocalizeConfig{
+		MessageID: key,
+	})
+	if err != nil {
+		return key
+	}
+	return fmt.Sprintf(msg, args...)
+}
+
+// ParseLanguageTag parses an Accept-Language header or locale string into a language tag
+func ParseLanguageTag(locale string) language.Tag {
+	if locale == "" {
+		return language.English
+	}
+
+	tag, err := language.Parse(locale)
+	if err != nil {
+		return language.English
+	}
+	return tag
+}
